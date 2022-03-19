d8dc9a5d4429e1b6e178ae4816d8fac588f65b95
JeffreyLove <84661537+Jeffrey-love@users.noreply.github.com>
2022-03-19 16:10:34 +0800

    Add files via upload

---
+++ Sanners/TCP_SYN+ACK.py
@@ -0,0 +1,28 @@
+import socket
+
+from whois import whois
+
+from scapy.all import *
+from random import randint
+import time
+
+
+def main():
+    ip = '192.168.153.135'
+    port = 3306
+    packet = IP(dst=ip)/TCP(sport=12345, dport=port, flags="S")
+    resp = sr1(packet, timeout=20)
+    if(str(type(resp)) == '<type "NoneType">'):
+        print("port %s is closed" %(port))
+    elif(resp.haslayer(TCP)):
+        # 判断是否有ACK值
+        if(resp.getlayer(TCP).flags == 0x12):
+            send_rst = sr(IP(dst=ip)/TCP(sport=12345, dport=80, flags="AR"), timeout=20)
+            print("port %s is open" %(port))
+        # 判断是否拒绝连接
+        elif(resp.getlayer(TCP).flags == 0x14):
+            print("prot %s is down" %(port))
+
+
+if __name__ == '__main__':
+    main()
